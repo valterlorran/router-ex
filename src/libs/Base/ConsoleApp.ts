afd0c7e9d2e7595bc67c0c77b24c58521bee59e5
Valter Lorran <valterlorran@gmail.com>
2020-11-16 22:11:33 -0300

    getting the signature

--- src/libs/Base/ConsoleApp.ts
+++ src/libs/Base/ConsoleApp.ts
@@ -3,24 +3,25 @@ import BaseApp from "./BaseApp";
 import { readdirSync } from "fs";
 import { Command } from "./Console/Command";
 import { Dictionary } from "libs/types";
+import path from 'path';
 
 export class ConsoleApp extends BaseApp {
     protected commands: Dictionary<typeof Command> = {};
 
 
     public handler() {
-        
+        this.registerCommands();
     }
 
     protected async load(folder: string) {
         const files:string[] = await readdirSync(folder);
 
         for (let index = 0; index < files.length; index++) {
-            const file = files[index];
+            const file = path.join(folder, files[index]);
             const command: any = (await import(file)).default;
-
             if (command.prototype instanceof Command) {
-                this.commands[(command as Command).getSignature()] = command;
+                const instance: Command = new command();
+                this.commands[instance.getSignature()] = command;
             } else {
                 console.log(file, 'Is not a Command');
             }
